2141339ca42f7e78b95b275beaa13cba766a4a53
uniboy <liu.xinzh@northeastern.edu>
2022-12-22 16:30:44 -0500

    full project

---
+++ src/main/java/com/uniboy/bean/StudentService.java
@@ -0,0 +1,41 @@
+package com.uniboy.bean;
+
+import com.uniboy.exception.BadRequestException;
+import com.uniboy.exception.StudentNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student) {
+        // check if email is taken
+        Boolean existsEmail = studentRepository
+                .selectExistsEmail(student.getEmail());
+        if (existsEmail) {
+            throw new BadRequestException(
+                    "Email " + student.getEmail() + " taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        // check if student exists
+        if(!studentRepository.existsById(studentId)) {
+            throw new StudentNotFoundException(
+                    "Student with id " + studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+}
